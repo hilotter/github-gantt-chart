9fb2cbe5da073fede33f044d2db7faa111208a8a
hilotter <hilotter3@gmail.com>
2020-04-05 12:11:38 +0900

    add redirect when already logged in

--- pages/login.vue
+++ pages/login.vue
@@ -17,9 +17,18 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState } from 'vuex'
 
 export default Vue.extend({
   auth: false,
+  computed: {
+    ...mapState('auth', ['loggedIn']),
+  },
+  created() {
+    if (this.loggedIn) {
+      this.$router.push('/gantt')
+    }
+  },
   methods: {
     authenticate() {
       this.$auth.loginWith('github')
